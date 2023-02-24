4bb81b5e128791600703e4891ca9b20558ae59de
Sergey Zubkov <ksevelyar@protonmail.com>
2023-02-25 00:02:43 +0300

    init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-unstable;
+    flake-utils.url = github:numtide/flake-utils;
+    rust-overlay.url = github:oxalica/rust-overlay;
+  };
+
+  outputs = { nixpkgs, flake-utils, rust-overlay, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+      in
+      {
+        devShell = pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              (
+                rust-bin.stable.latest.default.override {
+                  targets = [
+                    "thumbv7em-none-eabihf"
+                  ];
+                }
+              )
+              gdb
+              cargo-binutils
+              cargo-embed
+              minicom
+            ];
+          };
+      }
+    );
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+# Rust boilerplate for LSM303AGR
+
+## [Discovery](https://docs.rust-embedded.org/discovery/)
+
+## Setup
+
+`direnv allow`
+
+## Build
+
+`cargo build --target thumbv7em-none-eabihf`
